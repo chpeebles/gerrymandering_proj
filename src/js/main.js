83dd4ab5bd397f524319a3609eca031559b97df5
Christina Peebles <chrpeebles@gmail.com>
2021-03-30 14:39:04 -0500

    added city sections and jquery to toggle them

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,8 @@ const copyrightYear = document.querySelector('.copyright-year');
 const now = new Date();
 copyrightYear.innerHTML = now.getFullYear();
 
-// home page on click of what, why, how, hide the current active one
 jQuery(function() { 
+  // What Why How Events
   $("#what-event").on( "click", function () {
     $("#what-sect").show();
     $("#why-sect").hide();
@@ -35,6 +35,77 @@ jQuery(function() {
     $("#how-event").addClass("active");
     $("#why-event").removeClass("active");
     $("#what-event").removeClass("active");
+  });
+
+  // City Events
+  $("#austin-event").on( "click", function () {
+    $("#austin-sect").show();
+    $("#dallas-sect").hide();
+    $("#elPaso-sect").hide();
+    $("#houston-sect").hide();
+    $("#sanAntonio-sect").hide();
+
+    $("#austin-event").addClass("active");
+    $("#dallas-event").removeClass("active");
+    $("#elPaso-event").removeClass("active");
+    $("#houston-event").removeClass("active");
+    $("#sanAntonio-event").removeClass("active");
+  });
+
+  $("#dallas-event").on( "click", function () {
+    $("#dallas-sect").show();
+    $("#austin-sect").hide();
+    $("#elPaso-sect").hide();
+    $("#houston-sect").hide();
+    $("#sanAntonio-sect").hide();
+
+    $("#dallas-event").addClass("active");
+    $("#austin-event").removeClass("active");
+    $("#elPaso-event").removeClass("active");
+    $("#houston-event").removeClass("active");
+    $("#sanAntonio-event").removeClass("active");
+  });
+
+  $("#elPaso-event").on( "click", function () {
+    $("#elPaso-sect").show();
+    $("#austin-sect").hide();
+    $("#dallas-sect").hide();
+    $("#houston-sect").hide();
+    $("#sanAntonio-sect").hide();
+
+    $("#elPaso-event").addClass("active");
+    $("#austin-event").removeClass("active");
+    $("#dallas-event").removeClass("active");
+    $("#houston-event").removeClass("active");
+    $("#sanAntonio-event").removeClass("active");
+  });
+
+  $("#houston-event").on( "click", function () {
+    $("#houston-sect").show();
+    $("#dallas-sect").hide();
+    $("#elPaso-sect").hide();
+    $("#austin-sect").hide();
+    $("#sanAntonio-sect").hide();
+
+    $("#houston-event").addClass("active");
+    $("#dallas-event").removeClass("active");
+    $("#elPaso-event").removeClass("active");
+    $("#austin-event").removeClass("active");
+    $("#sanAntonio-event").removeClass("active");
+  });
+
+  $("#sanAntonio-event").on( "click", function () {
+    $("#sanAntonio-sect").show();
+    $("#dallas-sect").hide();
+    $("#elPaso-sect").hide();
+    $("#houston-sect").hide();
+    $("#austin-sect").hide();
+
+    $("#sanAntonio-event").addClass("active");
+    $("#dallas-event").removeClass("active");
+    $("#elPaso-event").removeClass("active");
+    $("#houston-event").removeClass("active");
+    $("#austin-event").removeClass("active");
   });
 
 });
