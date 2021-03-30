ed2a3a15e8d4247aa560977e705c4c557998a41a
Christina Peebles <chrpeebles@gmail.com>
2021-03-30 14:19:00 -0500

    added jquery to toggle why, what and how home page sections

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,37 @@
 const copyrightYear = document.querySelector('.copyright-year');
 const now = new Date();
 copyrightYear.innerHTML = now.getFullYear();
+
+// home page on click of what, why, how, hide the current active one
+jQuery(function() { 
+  $("#what-event").on( "click", function () {
+    $("#what-sect").show();
+    $("#why-sect").hide();
+    $("#how-sect").hide();
+
+    $("#what-event").addClass("active");
+    $("#why-event").removeClass("active");
+    $("#how-event").removeClass("active");
+  });
+
+  $("#why-event").on( "click", function () {
+    $("#why-sect").show();
+    $("#what-sect").hide();
+    $("#how-sect").hide();
+
+    $("#why-event").addClass("active");
+    $("#what-event").removeClass("active");
+    $("#how-event").removeClass("active");
+  });
+
+  $("#how-event").on( "click", function () {
+    $("#how-sect").show();
+    $("#what-sect").hide();
+    $("#why-sect").hide();
+
+    $("#how-event").addClass("active");
+    $("#why-event").removeClass("active");
+    $("#what-event").removeClass("active");
+  });
+
+});
