0131e144120733f261ce62f1e7320f2a243c9e2f
Christina Peebles <chrpeebles@gmail.com>
2021-04-12 11:58:11 -0500

    updated home state icons, added hover card styling to blog

--- src/js/main.js
+++ src/js/main.js
@@ -41,13 +41,11 @@ jQuery(function() {
   $("#austin-event").on( "click", function () {
     $("#austin-sect").show();
     $("#dallas-sect").hide();
-    $("#elPaso-sect").hide();
     $("#houston-sect").hide();
     $("#sanAntonio-sect").hide();
 
     $("#austin-event").addClass("active");
     $("#dallas-event").removeClass("active");
-    $("#elPaso-event").removeClass("active");
     $("#houston-event").removeClass("active");
     $("#sanAntonio-event").removeClass("active");
   });
@@ -55,27 +53,11 @@ jQuery(function() {
   $("#dallas-event").on( "click", function () {
     $("#dallas-sect").show();
     $("#austin-sect").hide();
-    $("#elPaso-sect").hide();
     $("#houston-sect").hide();
     $("#sanAntonio-sect").hide();
 
     $("#dallas-event").addClass("active");
     $("#austin-event").removeClass("active");
-    $("#elPaso-event").removeClass("active");
-    $("#houston-event").removeClass("active");
-    $("#sanAntonio-event").removeClass("active");
-  });
-
-  $("#elPaso-event").on( "click", function () {
-    $("#elPaso-sect").show();
-    $("#austin-sect").hide();
-    $("#dallas-sect").hide();
-    $("#houston-sect").hide();
-    $("#sanAntonio-sect").hide();
-
-    $("#elPaso-event").addClass("active");
-    $("#austin-event").removeClass("active");
-    $("#dallas-event").removeClass("active");
     $("#houston-event").removeClass("active");
     $("#sanAntonio-event").removeClass("active");
   });
@@ -83,13 +65,11 @@ jQuery(function() {
   $("#houston-event").on( "click", function () {
     $("#houston-sect").show();
     $("#dallas-sect").hide();
-    $("#elPaso-sect").hide();
     $("#austin-sect").hide();
     $("#sanAntonio-sect").hide();
 
     $("#houston-event").addClass("active");
     $("#dallas-event").removeClass("active");
-    $("#elPaso-event").removeClass("active");
     $("#austin-event").removeClass("active");
     $("#sanAntonio-event").removeClass("active");
   });
@@ -97,13 +77,11 @@ jQuery(function() {
   $("#sanAntonio-event").on( "click", function () {
     $("#sanAntonio-sect").show();
     $("#dallas-sect").hide();
-    $("#elPaso-sect").hide();
     $("#houston-sect").hide();
     $("#austin-sect").hide();
 
     $("#sanAntonio-event").addClass("active");
     $("#dallas-event").removeClass("active");
-    $("#elPaso-event").removeClass("active");
     $("#houston-event").removeClass("active");
     $("#austin-event").removeClass("active");
   });
